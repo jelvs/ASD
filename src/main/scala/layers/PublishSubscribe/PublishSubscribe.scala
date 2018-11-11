ae0fdd5eccbbc4c6d464903577e53c4a8e22c806
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2018-11-11 16:31:15 +0000

    Started implt publish/subs

--- src/main/scala/layers/PublishSubscribe/PublishSubscribe.scala
+++ src/main/scala/layers/PublishSubscribe/PublishSubscribe.scala
@@ -1,21 +1,74 @@
 package layers.PublishSubscribe
 
-import akka.actor.{Actor, Props}
+import java.util.concurrent.TimeUnit
+
+import akka.pattern.ask
+import akka.actor.{Actor, ActorRef, Props}
+import akka.util.Timeout
+import layers.EpidemicBroadcastTree.MainPlummtree.Broadcast
+import layers.MembershipLayer.PartialView
+import layers.MembershipLayer.PartialView.getPeers
+import layers.PublishSubscribe.PublishSubscribe.{NeighborSubscription, NeighborUnSubscription, Publish, UnSubscribe}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.FiniteDuration
 
 class PublishSubscribe  extends Actor
 {
 
+  val ACTOR_NAME: String = "/user/PublishSubscribe"
+
+  //TODO: Adicionar akka.tcp:// em tudo
+  val prependAkkaIdentifier :String = "akka.tcp://"
   var myNeighbors : List[String]
   var mySubscriptions : List[String]
   var neighborSubscriptions : Map[String, List[String]]
+  var ownAddress: String = ""
+  val fanout: Integer = 5
 
   override def receive: Receive = {
-    case init: PublishSubscribe.Init =>{
+    case _: PublishSubscribe.Init =>{
 
-
+      this.ownAddress = self.path.address.hostPort //returns as node@host:port
+      implicit val timeout = Timeout(FiniteDuration(1, TimeUnit.SECONDS))
+      val future = context.actorSelection("/user/PartialView").resolveOne()
+      val partialViewRef: ActorRef = Await.result(future, timeout.duration)
+      val future2 = partialViewRef ? getPeers(fanout)
+      myNeighbors = Await.result(future2, timeout.duration).asInstanceOf[List[String]]
 
     }
 
+    case subscribe: PublishSubscribe.Subcribe =>{
+
+      if(!mySubscriptions.contains(subscribe.topic)){
+        mySubscriptions = mySubscriptions :+ subscribe.topic
+        for(nei <- myNeighbors){
+          val actorRef = context.actorSelection(prependAkkaIdentifier.concat(nei.concat(ACTOR_NAME)))
+          val neiSub: NeighborSubscription = NeighborSubscription(subscribe.topic, ownAddress)
+          actorRef ! neiSub
+        }
+      }
+    }
+
+    case unSubscribe: UnSubscribe =>{
+
+      if(mySubscriptions.contains(unSubscribe.topic)){
+        mySubscriptions = mySubscriptions.filter(_ != unSubscribe.topic)
+        for(nei <- myNeighbors){
+          val actorRef = context.actorSelection(prependAkkaIdentifier.concat(nei.concat(ACTOR_NAME)))
+          val neiUnsub: NeighborUnSubscription = NeighborUnSubscription(unSubscribe.topic, ownAddress)
+          actorRef ! neiUnsub
+        }
+      }
+    }
+
+    case publish: Publish => {
+
+      val broadcastTree = context.actorSelection("/user/MainPlummtree")
+      broadcastTree ! Broadcast(publish)
+    }
+
+      
   }
 }
 
